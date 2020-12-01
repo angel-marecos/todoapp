f309be561f3f3d3dd5bf7d1537b3ad9f3b22ced7
Angel <angel.marecos@fiuni.edu.py>
2020-12-01 13:34:02 -0300

    master: restructured files
    remove and add new files

--- backend/config/schema.py
+++ backend/config/schema.py
@@ -1,14 +1,14 @@
 import graphene
-import tasks.schema
-import users.schema
+import apps.tasks.schema
+import apps.users.schema
 import graphql_jwt
 
 
-class Query(users.schema.Query, tasks.schema.Query, graphene.ObjectType):
+class Query(apps.users.schema.Query, apps.tasks.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(users.schema.Mutation, tasks.schema.Mutation, graphene.ObjectType):
+class Mutation(apps.users.schema.Mutation, apps.tasks.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
